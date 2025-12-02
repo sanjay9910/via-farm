f44b4ea411b20d60edd728a45c9f33ffb190f003
devesh-qurilo <devesh@qurilo.com>
2025-12-02 13:00:18 +0530

    sanjay update

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -276,8 +276,8 @@ import { saveToken } from "../utility/Storage";
 
 export default function LoginScreen() {
   const navigation = useNavigation();
-  const [mobile, setMobile] = useState("7777777777");
-  const [password, setPassword] = useState("123456");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
   const { login, user } = useContext(AuthContext);
 
   const handleLogin = async () => {
